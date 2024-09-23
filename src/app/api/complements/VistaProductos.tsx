ea21c61bed13d19e2cec0a067433163fded72969
moyano18 <rubenmoyano_2000@hotmail.com>
2024-09-23 09:56:53 -0300

    page vistaproductos

---
+++ src/app/api/complements/VistaProductos.tsx
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server"
+import { prisma } from "@/libs/prisma"
+import React from 'react'
+
+const VistaProductos = () => {
+  return (
+    <div>VistaProductos</div>
+  )
+}
+
+export default VistaProductos
