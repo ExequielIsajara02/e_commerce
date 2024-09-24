df1797f837d0b025dff5443db06bcd1340a81940
moyano18 <rubenmoyano_2000@hotmail.com>
2024-09-24 10:22:19 -0300

    page vistaproductos en formato carta

--- src/app/api/complements/VistaProductos.tsx
+++ src/app/api/complements/VistaProductos.tsx
@@ -1,10 +1,42 @@
+"use client"
 import { NextResponse } from "next/server"
 import { prisma } from "@/libs/prisma"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const VistaProductos = () => {
+
+    const [productos, setProductos] = useState([])
+
+    const traerProductos = async () => {
+        const respuesta = await fetch("http://localhost:3000/api/producto")
+        const datos = await respuesta.json()
+        setProductos(datos)
+        console.log(datos);
+        
+        return datos
+    }
+
+    useEffect(() => {
+        traerProductos()
+    }, [])
   return (
-    <div>VistaProductos</div>
+    <>
+        {productos.map((producto) => {
+
+            return (
+                <div className="grid grid-cols-4">
+                {productos.map((producto: { id: number, nombre: string, descripcion: string, precio: number }) => (
+  <div className="border-black border rounded-lg m-4" key={producto.id}>
+    <p className="mb-1 mx-4 my-1">{producto.nombre}</p>
+    <p className="mb-1 mx-4 my-1">{producto.descripcion}</p>
+    <p className="mb-1 mx-4 my-1">${producto.precio}</p>
+    <button className="bg-blue-500 p-1 rounded-lg text-white border border-black">Agregar al carrito</button>
+                  </div>
+                ))}
+              </div>
+            )
+        })}
+    </>
   )
 }
 
