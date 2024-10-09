e14b6f14f32f9d31572b0b18550a4c8db222a42f
Andres Netbook <andres.silva41202@gmail.com>
2024-10-09 12:40:13 -0300

    cambios page success

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,17 +1,23 @@
 import { Pedido } from "@prisma/client";
 import DetallePedido from "@/components/DetallePedido";
 import { prisma } from "@/libs/prisma";
+// import EnviarPdf from "./EnviarPdf";
 
 export default async function Page() {
-  const pedidos: Pedido[] = await prisma.pedido.findMany();
+  const pedidos = await prisma.pedido.findMany({
+    include: {
+      productos: {
+        include: {
+          producto: true,
+        },
+      },
+    },
+  });
+
   return (
     <div>
-      
-      {/* se renderiza el componente DetallePedido */}
-      <DetallePedido  pedidos={pedidos}/>
+      <DetallePedido pedidos={pedidos}/>
+      {/* <EnviarPdf pedidos={pedidos}/> */}
     </div>
-
   );
 }
-
-  
--- src/components/DetallePedido.tsx
+++ src/components/DetallePedido.tsx
@@ -1,14 +1,22 @@
-
-import { prisma } from "@/libs/prisma";
 import { Pedido } from "@prisma/client";
+
+// Importamos el tipo de PedidoProducto para utilizarlo en las props
 interface DetallePedidoProps {
-  pedidos: Pedido[];
+  pedidos: (Pedido & {
+    productos: {
+      cantidad: number; // La cantidad del producto en el pedido
+      producto: {
+        nombre: string; // Nombre del producto
+        precio: number; // Precio del producto
+      };
+    }[];
+  })[];
 }
+
 export default async function DetallePedido({ pedidos }: DetallePedidoProps) {
   
   return (
     <div className="bg-slate-300 text-black flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-
       <h1 className="text-2xl text-center">Detalle del Pedido</h1>
       <div className="py-8">
         {pedidos.length > 0 ? (
@@ -19,18 +27,24 @@ export default async function DetallePedido({ pedidos }: DetallePedidoProps) {
             >
               <h2 className="text-lg font-bold">Pedido número: {pedido.id_pedido}</h2>
               <p>Fecha: {new Date(pedido.fecha).toLocaleDateString()}</p>
-              <p>Cantidad: {pedido.cantidad}</p>
               <p>Método de Pago: {pedido.metodo_pago}</p>
               <p>Estado: {pedido.estado}</p>
               <p>Precio Final: ${pedido.precio_final}</p>
+
+              {/* Aquí mostramos los productos y sus cantidades */}
+              <h3 className="text-lg font-semibold">Productos:</h3>
+              {pedido.productos.map((pedidoProducto) => (
+                <div key={pedidoProducto.producto.nombre}>
+                  <p>{pedidoProducto.producto.nombre}: {pedidoProducto.cantidad} unidades</p>
+                </div>
+              ))}
             </div>
           ))
         ) : (
           <p>No hay pedidos disponibles.</p>
         )}
       </div>
-      
-      {/* <p> El total pagado es ${Total}</p> */}
     </div>
   )
 }
+ 
