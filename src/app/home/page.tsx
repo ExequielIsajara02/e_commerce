59c57aad4554d26908ce307baa4c5ee954642783
Exequiel Isajara <exequielisajara@gmail.com>
2024-10-16 16:55:18 -0300

    authorization

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,4 +1,5 @@
 import { Header } from "@/components/Header";
+import { useRouter } from "next/navigation";
 import VistaProductos from "./VistaProductos";
 import { auth } from "@/auth";
 
@@ -6,10 +7,16 @@ import { auth } from "@/auth";
 const Page = async () => {
 
     const session = await auth();
+    const router = useRouter();
 
-    if(session?.user === null) {
+    /*if(session?.user === null) {
         return null;
-    }
+    } */
+
+    console.log(session);
+
+    // Crear un array de string con los roles
+
 
     return (
         <div>
