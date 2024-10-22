c464e7ccf385523dd2052b67c4cefd7caea6b53a
Andres Netbook <andres.silva41202@gmail.com>
2024-10-22 11:03:35 -0300

    fix home page errores

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,5 @@
 import { Header } from "@/components/Header";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import VistaProductos from "./VistaProductos";
 import { auth } from "@/auth";
 
@@ -7,13 +7,13 @@ import { auth } from "@/auth";
 const Page = async () => {
 
     const session = await auth();
-    const router = useRouter();
+    // const router = useRouter();
 
     /*if(session?.user === null) {
         return null;
     } */
 
-    console.log(session);
+    // console.log(session);
 
     // Crear un array de string con los roles
 
@@ -21,7 +21,7 @@ const Page = async () => {
     return (
         <div>
             <h2>Mostrar productos</h2>
-            <pre>{JSON.stringify(session, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(session, null, 2)}</pre> */}
             <VistaProductos />
         </div>
     );
